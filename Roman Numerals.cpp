dd11134e91d7aa67a0f3c2924dd1d9917c39845c
KonainRaza01 <131591948+KonainRaza01@users.noreply.github.com>
2023-07-08 21:57:57 +0500

    Roman Numerals.cpp

--- Roman Numerals.cpp
+++ Roman Numerals.cpp
@@ -1,31 +1,86 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main() {
-    int number;
-    cout << "Enter a Number: ";
-    cin >> number;
-
-    string roman;
-
-    const int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    const string numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-	if(number>0){
-	
-    for (int i = 0; i < 13; ++i) {
-        while (number >= values[i]) {
-            roman += numerals[i];
-            number -= values[i];
-        }
+void convertToRoman(int num) {
+    if (num <= 0 || num > 3999) {
+        cout << "Invalid number. Please enter a number between 1 and 3999." << endl;
+        return;
     }
 
-    cout << "Roman numeral: " << roman << endl;
-}
-else{
-	cout<< "Negative numbers are not allowed."<<endl;
-}
-    return 0;
+    cout << "Roman Numeral: ";
+
+    while (num >= 1000) {
+        cout << "M";
+        num -= 1000;
+    }
+
+    if (num >= 900) {
+        cout << "CM";
+        num -= 900;
+    }
+
+    if (num >= 500) {
+        cout << "D";
+        num -= 500;
+    }
+
+    if (num >= 400) {
+        cout << "CD";
+        num -= 400;
+    }
+
+    while (num >= 100) {
+        cout << "C";
+        num -= 100;
+    }
+
+    if (num >= 90) {
+        cout << "XC";
+        num -= 90;
+    }
+
+    if (num >= 50) {
+        cout << "L";
+        num -= 50;
+    }
+
+    if (num >= 40) {
+        cout << "XL";
+        num -= 40;
+    }
+
+    while (num >= 10) {
+        cout << "X";
+        num -= 10;
+    }
+
+    if (num >= 9) {
+        cout << "IX";
+        num -= 9;
+    }
+
+    if (num >= 5) {
+        cout << "V";
+        num -= 5;
+    }
+
+    if (num >= 4) {
+        cout << "IV";
+        num -= 4;
+    }
+
+    while (num >= 1) {
+        cout << "I";
+        num -= 1;
+    }
+
+    cout << endl;
 }
 
+int main() {
+    int num;
+    cout << "Enter the Number: ";
+    cin >> num;
+    convertToRoman(num);
+    return 0;
+}
